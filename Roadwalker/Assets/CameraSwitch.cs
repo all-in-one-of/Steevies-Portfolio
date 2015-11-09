e0ef38f792ef6f32accb893a31de9b72dc4b300b
Beth Seethaler <beth.seethaler@gmail.com>
2015-11-09 10:58:21 -0700

    Got UI Working
    UI is now working. Just need to add everything.

--- Roadwalker/Assets/CameraSwitch.cs
+++ Roadwalker/Assets/CameraSwitch.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class CameraSwitch : MonoBehaviour {
 
+    public Vector3 newPosition;
+    public Transform newRot;
+
+    public float smoothing;
+
+    public GameObject theCamera; 
+
 	// Use this for initialization
 	void Start () {
+        
+        theCamera = GameObject.Find("Main Camera");
+        smoothing = 2.0f;
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void SwithPositions()
+    {
+        StartCoroutine( MoveToPosition (theCamera, newPosition, newRot));
+        StopAllCoroutines();
+
+    }
+
+    IEnumerator MoveToPosition(GameObject viewer, Vector3 target, Transform newRotation)
+    {
+        while (Vector3.Distance(viewer.transform.position, target) > 0.05f)
+        {
+            viewer.transform.position = target; //Vector3.Lerp(transform.position, target, Time.deltaTime);
+            viewer.transform.LookAt(newRotation);
+
+            yield return null;
+        }
+
+        yield return null;
+    }
 }
