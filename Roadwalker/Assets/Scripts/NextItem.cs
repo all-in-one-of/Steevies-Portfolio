7b80c74ef9aa3e68f2e84714834121b562cc8732
Stephen Larsen <maxpendragon@gmail.com>
2015-11-09 13:00:19 -0700

    Corrected stuff.

---
+++ Roadwalker/Assets/Scripts/NextItem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextItem : MonoBehaviour {
+
+	public GameObject[] pObjects;
+	public int currentObject;
+
+	// Use this for initialization
+	void Start () {
+		currentObject = 0;
+		pObjects [currentObject].SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NextObject (){
+		if (currentObject < pObjects.Length) {
+			pObjects[currentObject].SetActive(false);
+			currentObject++;
+			pObjects[currentObject].SetActive(true);
+		}
+	}
+
+	public void PreviousObject() {
+		if (currentObject > 0) {
+			pObjects[currentObject].SetActive(false);
+			currentObject--;
+			pObjects[currentObject].SetActive(true);
+		}
+	}
+}
